f0f22d9dfde0091a88c9d37372cdb39df4eb0d23
Vincent De Smet <vincent.desmet@honestbee.com>
2017-04-16 20:16:15 +0800

    Add HomeController FluentAssertions for About View

---
+++ BasicAppTests/Controllers/HomeControllerFacts.cs
@@ -0,0 +1,35 @@
+using Xunit;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using BasicApp.Controllers;
+
+namespace BasicAppTests
+{
+
+    public class HomeControllerFacts
+    {
+        [Fact]
+        public void About_ReturnsAViewResult_WithAMessage()
+        {
+            // Instantiate
+            var controller = new HomeController();
+
+            // Call
+            var aboutResult = controller.About();
+
+            //Verify Message Exists
+            aboutResult.Should().BeOfType<ViewResult>()
+                .Which.ViewData.Should().ContainKey("Message");
+        }
+
+        [Fact]
+        public void HomeControllerFailingTest(){
+            Add(2,3).Should().Be(4);
+        }
+
+        int Add(int x, int y){
+            return x + y;
+        }
+    }
+}
